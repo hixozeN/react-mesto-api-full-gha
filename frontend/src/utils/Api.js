ac606b14193a6c12fe91a892bb77e12e3a1b599e
Vladislav K <aljkeee@gmail.com>
2023-05-13 00:36:26 +0300

    Изменяет заголовки

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,6 +1,6 @@
 class Api {
   constructor() {
-    this._link = 'https://api.mesto.hixozen.ru';
+    this._link = "https://api.mesto.hixozen.ru";
     // this._link = 'http://localhost:3000';
   }
 
@@ -9,124 +9,116 @@ class Api {
       return res.json();
     }
     return Promise.reject(`Ошибка: ${res.status}`);
-  };
+  }
 
   getUserInfo() {
     return fetch(`${this._link}/users/me`, {
       headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${ localStorage.getItem('jwt') }`,
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        Accept: "*/*",
       },
-    })
-      .then(res => this._checkResponse(res))
-  };
+    }).then((res) => this._checkResponse(res));
+  }
 
-  setUserInfo({name, about}) {
+  setUserInfo({ name, about }) {
     return fetch(`${this._link}/users/me `, {
       headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${ localStorage.getItem('jwt') }`,
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
       },
-      method: 'PATCH',
+      method: "PATCH",
       body: JSON.stringify({
-        name, about
-      })
-    })
-      .then(res => this._checkResponse(res))
-  };
+        name,
+        about,
+      }),
+    }).then((res) => this._checkResponse(res));
+  }
 
-  setUserAvatar({avatar}) {
+  setUserAvatar({ avatar }) {
     return fetch(`${this._link}/users/me/avatar`, {
       headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${ localStorage.getItem('jwt') }`,
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
       },
-      method: 'PATCH',
-      body: JSON.stringify({avatar})
-    })
-      .then(res => this._checkResponse(res))
-  };
+      method: "PATCH",
+      body: JSON.stringify({ avatar }),
+    }).then((res) => this._checkResponse(res));
+  }
 
   getInitialCards() {
     return fetch(`${this._link}/cards`, {
       headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${ localStorage.getItem('jwt') }`,
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        Accept: "*/*",
       },
-    })
-      .then(res => this._checkResponse(res))
-  };
+    }).then((res) => this._checkResponse(res));
+  }
 
-  createCard({name, link}) {
+  createCard({ name, link }) {
     return fetch(`${this._link}/cards`, {
       headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${ localStorage.getItem('jwt') }`,
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
       },
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
-        name, link
-      })
-    })
-      .then(res => this._checkResponse(res))
-  };
+        name,
+        link,
+      }),
+    }).then((res) => this._checkResponse(res));
+  }
 
   deleteCard(cardId) {
     return fetch(`${this._link}/cards/${cardId}`, {
       headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${ localStorage.getItem('jwt') }`,
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
       },
-      method: 'DELETE',
-
-    })
-      .then(res => this._checkResponse(res))
-  };
+      method: "DELETE",
+    }).then((res) => this._checkResponse(res));
+  }
 
   putCardLike(cardId) {
     return fetch(`${this._link}/cards/${cardId}/likes`, {
       headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${ localStorage.getItem('jwt') }`,
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
       },
-      method: 'PUT'
-    })
-      .then(res => this._checkResponse(res))
-  };
+      method: "PUT",
+    }).then((res) => this._checkResponse(res));
+  }
 
   deleteCardLike(cardId) {
     return fetch(`${this._link}/cards/${cardId}/likes`, {
       headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${ localStorage.getItem('jwt') }`,
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
       },
-      method: 'DELETE'
-    })
-      .then(res => this._checkResponse(res))
-  };
+      method: "DELETE",
+    }).then((res) => this._checkResponse(res));
+  }
 
   changeLikeCardStatus(cardId, isLiked) {
     if (isLiked) {
       return fetch(`${this._link}/cards/${cardId}/likes`, {
         headers: {
-          'Content-Type': 'application/json',
-          authorization: `Bearer ${ localStorage.getItem('jwt') }`,
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
         },
-        method: 'DELETE'
-      })
-        .then(res => this._checkResponse(res)) 
+        method: "DELETE",
+      }).then((res) => this._checkResponse(res));
     } else {
       return fetch(`${this._link}/cards/${cardId}/likes`, {
         headers: {
-          'Content-Type': 'application/json',
-          authorization: `Bearer ${ localStorage.getItem('jwt') }`,
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("jwt")}`,
         },
-        method: 'PUT'
-      })
-        .then(res => this._checkResponse(res))
+        method: "PUT",
+      }).then((res) => this._checkResponse(res));
     }
   }
-
 }
 
 const api = new Api();
