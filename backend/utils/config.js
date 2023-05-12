1db98c3cce481d47512ea84c066c85a3de6c9a0b
Vladislav K <aljkeee@gmail.com>
2023-05-12 22:04:10 +0300

    Изменяет env-окружение

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,7 +1,7 @@
 const SECRET = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev';
-const MONGO = process.env.MONGO_DB;
+const { MONGO_DB } = process.env.MONGO_DB;
 const PORT = process.env.PORT || 3000;
 
 module.exports = {
-  SECRET, MONGO, PORT,
+  SECRET, MONGO_DB, PORT,
 };
